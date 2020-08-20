a36f4d88ac581ea2c85c8aea8e9b7bc6baef4461
Polina Skoraya <skorayapolina@gmail.com>
2020-08-20 11:03:32 +0300

    finish table resize

---
+++ src/components/table/table.resize.js
@@ -0,0 +1,51 @@
+import {$} from '@core/dom';
+
+export function resizeHandler($root, event) {
+  const $resizer = $(event.target);
+  const $parent = $resizer.closest('[data-type="resizable"]');
+  const coords = $parent.getCoords();
+  const resizeType = $resizer.data.resize;
+  const sideProp = resizeType === 'col' ? 'bottom' : 'right';
+  let value = '';
+
+  let cellsToResize = [];
+  if (resizeType === 'col') {
+    cellsToResize = $root.findAll(`[data-${resizeType}="${$parent.data.col}"]`);
+  }
+
+  $resizer.css({
+    [sideProp]: '-5000px'
+  });
+
+  document.onmousemove = e => {
+    if (resizeType === 'col') {
+      const delta = e.pageX - coords.right;
+      value = e.clientX - coords.left;
+      $resizer.css({
+        right: -delta + 'px'
+      });
+    } else {
+      const delta = e.pageY - coords.bottom;
+      value = e.clientY - coords.top;
+      $resizer.css({
+        bottom: -delta + 'px'
+      });
+    }
+  }
+
+  document.onmouseup = () => {
+    document.onmousemove = null;
+    document.onmouseup = null;
+    if (resizeType === 'col') {
+      $parent.css({width: value + 'px'});
+      cellsToResize.forEach(el => el.style.width = value + 'px');
+    } else {
+      $parent.css({height: value + 'px'});
+    }
+
+    $resizer.css({
+      right: '0',
+      bottom: '0'
+    });
+  }
+}
